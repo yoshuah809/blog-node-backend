d87500b7278b5ebc86cf49384ae7341849160c15
Josue <yoshuah809@gmail.com>
2022-09-16 14:32:12 -0500

    created post endpoint to update upvotes and created dev script to run dev short version

--- src/server.js
+++ src/server.js
@@ -1,11 +1,23 @@
-import express, { response } from "express";
+import express from "express";
 
+let articlesInfo = [
+	{ name: "learn-react", upvotes: 0 },
+	{ name: "learn-node", upvotes: 0 },
+	{ name: "learn-vue", upvotes: 0 },
+];
 const app = express();
 
-//hello
+app.use(express.json());
 
-app.post("/hello", (req, res) => {
-	res.send("Hello!");
+app.put("/api/articles/:name/upvote", (req, res) => {
+	const { name } = req.params;
+	const article = articlesInfo.find((a) => a.name === name);
+	if (article) {
+		article.upvotes += 1;
+		res.send(`The ${name} article now has ${article.upvotes} upvotes!!!`);
+	} else {
+		res.send("That article does not exist");
+	}
 });
 
 app.listen(8000, () => {
